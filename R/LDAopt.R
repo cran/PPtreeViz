45d56361659ce80b70fba7279c834683e0de0f0d
Eun-Kyung Lee <lee.eunk@gmail.com>
2015-05-08 00:00:00 +0000

    version 1.0.1

---
+++ R/LDAopt.R
@@ -0,0 +1,59 @@
+#' PP optimization using LDA index
+#' 
+#' Find the q-dim optimal projection using LDA projectin pursuit index
+#' @usage LDAopt(origclass, origdata,  q=1, weight = TRUE,...) 
+#' @param origclass class information vector
+#' @param origdata data matrix without class information
+#' @param q dimension of projection matrix
+#' @param weight weight flag using in LDA index
+#' @param ... arguments to be passed to methods
+#' @return indexbest maximum LDA index value
+#' @return projbest optimal q-dim projection matrix
+#' @return origclass original class information vector
+#' @return origdata  original data matrix  without class information
+#' @references Lee, EK., Cook, D., Klinke, S., and Lumley, T.(2005) 
+#' Projection Pursuit for exploratory supervised classification, 
+#' Journal of Computational and Graphical statistics, 14(4):831-846.
+#' @export
+#' @keywords projection pursuit
+#' @examples
+#' data(iris)
+#' LDA.proj.result <- LDAopt(iris[,5],iris[,1:4])
+#' LDA.proj.result$indexbest
+#' LDA.proj.result$projbest
+#' @useDynLib PPtreeViz
+LDAopt<-function(origclass,origdata,q=1,weight=TRUE,...) 
+{ 
+   origdata<-as.matrix(origdata)
+   class.table<-table(origclass)
+   class.name<-names(class.table)
+   p<-ncol(origdata)
+   mean.g<-matrix(apply(origdata,2,function(x) 
+                  tapply(x,origclass,mean,na.rm=TRUE)),ncol=p)
+   mean.all<-matrix(apply(origdata,2,mean),ncol=p)
+                   
+   B<-matrix(0,ncol=p,nrow=p)
+   W<-matrix(0,ncol=p,nrow=p)
+   g<-length(class.table)
+   n<-length(origclass)
+   
+   for(i in 1:g)
+   {  sel.id<-which(origclass==class.name[i])
+      temp.m1<-mean.g[i,]-mean.all
+      temp.m2<-origdata[sel.id,]-
+                matrix(1,length(sel.id),ncol=1)%*%mean.g[i,,drop=FALSE]      
+      gn1<-ifelse(weight,length(sel.id),n/g)
+      B<-B+gn1*t(temp.m1)%*%temp.m1
+      W<-W+gn1*t(temp.m2)%*%temp.m2/length(sel.id)
+   }
+   
+   opt<-eigen(MASS::ginv(W+B)%*%B)  
+   optVector<-matrix(as.numeric(opt$vectors[,1:q]),ncol=q)
+   proj.data<-origdata%*%optVector
+   optindex<-LDAindex(origclass,origdata,optVector,weight)  
+   optobj<-list(indexbest=optindex,projbest=optVector,
+                origclass = origclass,origdata= origdata)
+   class(optobj)<-append(class(optobj),"PPoptim")
+   return(optobj)
+
+}
---
+++ R/PPoptViz.R
@@ -0,0 +1,76 @@
+#' Visualize PPopt
+#' 
+#' Visualize the result of projection pursuit optimization
+#' @usage PPopt.Viz(PPoptOBJ)
+#' @param PPoptOBJ result from LDAopt, PDAopt, and PPopt
+#' @references Lee, EK., Cook, D., Klinke, S., and Lumley, T.(2005) 
+#' Projection Pursuit for exploratory supervised classification, 
+#' Journal of Computational and Graphical statistics, 14(4):831-846.
+#' @export
+#' @keywords tree
+#' @examples
+#' data(iris)
+#' PPopt.Viz(LDAopt(iris[,5],iris[,1:4],q=1))
+#' PPopt.Viz(LDAopt(iris[,5],iris[,1:4],q=2))
+#' @import ggplot2 grid gridExtra reshape2
+
+PPopt.Viz<-function(PPoptOBJ){
+
+   proj.data<-PPoptOBJ$origdata%*%PPoptOBJ$projbest
+   q<-ncol(proj.data)
+   p<-ncol(PPoptOBJ$origdata)   
+   vID <-1:p
+   if(q==1){
+      ..density..<-NULL
+      origclass<-PPoptOBJ$origclass
+      plot.data<-data.frame(proj.data,origclass)
+      p1<-ggplot(plot.data,aes(x=proj.data,group=origclass))+
+                geom_histogram(aes(y=..density..,fill=origclass))
+      coef<-PPoptOBJ$projbest[,1]
+      coef.data<-data.frame(vID,coef)
+      bin.width<-ifelse(p>100,1,0.1)
+      y.max<-max(c(abs(coef.data$coef),1/sqrt(p)))
+      y.min<- -y.max
+      p2<-ggplot(coef.data,aes(x=vID,y=coef))+        
+          geom_segment(aes(yend=0,xend=vID,width=0.1))+
+          geom_hline(yintercept=0)+
+          ylim(y.min,y.max) +
+          xlab("variable ID")+
+          ggtitle("Coefficients of Best Projection")
+      gridExtra::grid.arrange(p2,p1,nrow=1)   
+   } else{
+      plot.list<-list()
+      list.id<-1
+      for(i in 1:q){
+         for(j in 1:q){
+            if(i==j)
+            {  coef<-PPoptOBJ$projbest[,i]
+               coef.data<-data.frame(vID,coef)
+               bin.width<-ifelse(p>100,1,0.1)
+               y.max<-max(c(abs(coef.data$coef),1/sqrt(p)))
+
+               plot.list[[list.id]]<-ggplot(coef.data,aes(x=vID,y=coef))+
+                                     geom_segment(aes(yend=0,xend=vID,width=0.1))+
+                                     geom_hline(yintercept=0)+ 
+                                     xlab("variable ID")+ylim(-y.max,y.max)+
+                                     ggtitle(paste(
+                                        "Coefficients of Best Projection - dim",
+                                         as.character(i),sep=""))
+               list.id<-list.id+1
+            } else
+            {  x<-proj.data[,j]
+               y<-proj.data[,i]
+               origclass<-PPoptOBJ$origclass
+               plot.data<-data.frame(x,y,origclass)
+               plot.list[[list.id]]<-ggplot(plot.data,aes(x=x,y=y,
+                                                          color=origclass))+
+                                       geom_point()+
+                                       xlab(paste("dim",as.character(j)))+
+                                       ylab(paste("dim",as.character(i)))                 
+               list.id<-list.id+1 
+            }
+         }    
+      }
+      do.call(grid.arrange,plot.list)
+   }
+}
